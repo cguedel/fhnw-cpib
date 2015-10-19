ff0348a1ceb024719d97dfeff01191653ec4075a
PitchBlack <mail@rudolfvonrohr.net>
2015-10-19 21:26:29 +0200

    Tokens2
    Changes in the Tokens2 file

--- IML/Tokens2.hs
+++ IML/Tokens2.hs
@@ -1,68 +1,69 @@
-module IML.Tokens where
+module IML.Tokens2 where
 
-type Token = (Terminal, Maybe Attribute)
+  import IML.Enumerations
 
-data Terminal
- = LITERAL
-  | IDENT
-  | LPAREN -- (
-  | RPAREN -- )
-  | COMMA -- ,
-  | SEMICOLON -- ;
-  | COLON -- :
-  | BECOMES -- :=
-  | MULTOPR -- TIMES -- *
-  | ADDOPR PLUS -- +
-  | ADDOPR MINUS -- -
-  | RELOPR EQ -- =
-  | RELOPR NE -- /=
-  | RELOPR LT -- <
-  | RELOPR GT -- >
-  | RELOPR LE -- <=
-  | RELOPR GE -- >=
-  | BOOLOPR AND -- && (omit)
-  | BOOLOPR OR -- || (omit)
-  | BOOLOPR CAND -- &?
-  | BOOLOPR COR -- |?
-  | TYPE BOOL -- bool
-  | CALL -- call
-  | CHANGEMODE CONST -- const
-  | MECHMODE COPY -- const
-  | DEBUGIN -- debugin
-  | DEBUGOUT -- debugout
-  | MUTLTOPR DIV_E -- divE
-  | DO -- do
-  | ELSE -- else
-  | ENFUN -- endfun
-  | ENDIF -- endif
-  | ENDPROC -- endproc
-  | ENDPROGRAM -- endprogram
-  | ENDWHILE -- endwhile
-  | LITERAL BoolVal false -- false
-  | FUN -- fun
-  | GLOBAL -- global
-  | IF -- if
-  | FLOWMODE IN -- [in]
-  | INIT -- init
-  | FLOWMODE INOUT -- [inout]
-  | TYPE INT64 -- INT64
-  | LOCAL -- local
-  | MULTIOPR MOD_E -- modE
-  | NOTOPR -- not
-  | FLOWMODE OUT -- [out]
-  | PROC -- proc
-  | PROGRAM -- program
-  | MECHMODE REF -- ref
-  | RETURNS -- returns
-  | SKIP -- skip
-  | THEN -- then
-  | LITERAL BoolVal true -- true
-  | CHANGEMODE VAR -- var
-  | WHILE -- while
+  type Token = (Terminal, Maybe Attribute)
+
+  data Terminal
+    = LITERAL
+ --  | LITERAL BoolVal true -- true
+ --  | LITERAL BoolVal false -- false
+     | IDENT
+     | LPAREN -- (
+     | RPAREN -- )
+     | COMMA -- ,
+     | SEMICOLON -- ;
+     | COLON -- :
+     | BECOMES -- :=
+     | MULTOPR -- TIMES -- *
+--    | MULTIOPR MOD_E -- modE
+--  | MUTLTOPR DIV_E -- divE
+     | ADDOPR -- PLUS -- +
+--  | ADDOPR MINUS -- -
+     | RELOPR -- EQ -- =
+--  | RELOPR NE -- /=
+--  | RELOPR LT -- <
+--  | RELOPR GT -- >
+--  | RELOPR LE -- <=
+--  | RELOPR GE -- >=
+     | BOOLOPR -- AND -- && (omit)
+--  | BOOLOPR OR -- || (omit)
+--  | BOOLOPR CAND -- &?
+--  | BOOLOPR COR -- |?
+     | TYPE -- BOOL -- bool
+--    | TYPE INT64 -- INT64
+     | CALL -- call
+     | CHANGEMODE -- CONST -- const
+  --  | CHANGEMODE VAR -- var
+     | MECHMODE --COPY -- const
+  --  | MECHMODE REF -- ref
+     | DEBUGIN -- debugin
+     | DEBUGOUT -- debugout
+     | DO -- do
+     | ELSE -- else
+     | ENFUN -- endfun
+     | ENDIF -- endif
+     | ENDPROC -- endproc
+     | ENDPROGRAM -- endprogram
+     | ENDWHILE -- endwhile
+     | FUN -- fun
+     | GLOBAL -- global
+     | IF -- if
+     | FLOWMODE -- IN -- [in]
+  --  | FLOWMODE -- INOUT -- [inout]
+  --  | FLOWMODE OUT -- [out]
+     | INIT -- init
+     | LOCAL -- local
+     | NOTOPR -- not
+     | PROC -- proc
+     | PROGRAM -- program
+     | RETURNS -- returns
+     | SKIP -- skip
+     | THEN -- then
+     | WHILE -- while
     deriving (Show, Eq)
 
-data Attribute
-  = ALitAttrib Int
-  | BLitAttrib Bool
-  | IdentAttrib Ident
-  | ROprAttrib Opr
+  data Attribute
+    = ALitAttrib Int
+    | BLitAttrib Bool
+    | IdentAttrib Ident
