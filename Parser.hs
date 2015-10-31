5dc532030c247fc423b60f76c6c9f78f2fa0f337
PitchBlack <mail@rudolfvonrohr.net>
2015-10-31 10:51:13 +0100

    Last lection

--- Parser.hs
+++ Parser.hs
@@ -3,3 +3,12 @@
 -- o List with all Nonterminal - Symbol
 --
 -- Do this shit with Pattern matching Jack!
+
+--Nullable
+
+--- fuck das geit nit wo eg wett mache!!!
+nullable1 ::  -> Expression -> Bool
+
+nullable1 Epsilon _       = true
+nullable1 ts _            = false
+nullable1 nts Expression  = nullable1 nts
