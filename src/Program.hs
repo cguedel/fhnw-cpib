1726bd225ff0b63bb66ad55719ccd919bccc347f
Christian Güdel <cg@dmesg.ch>
2015-12-01 13:44:28 +0100

    Command line arguments

--- src/Program.hs
+++ src/Program.hs
@@ -1,5 +1,17 @@
 import Scanner
+import System.Environment
+import System.Directory
 
 main :: IO()
-main = print $ tokenize "+=<=\n\
-  \>=<=a134 734'000/123'000 bool"
+main = getArgs >>= parse
+
+parse :: [String] -> IO ()
+parse [] = error "Invalid cmdline arguments, usage: iml.exe <file>"
+parse f = do
+  exists <- doesFileExist $ head f
+  if exists then
+    do
+      contents <- readFile $ head f
+      print $ tokenize contents
+  else
+    error "File does not exist"
