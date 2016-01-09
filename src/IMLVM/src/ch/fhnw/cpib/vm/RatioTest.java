b7a8ecac3f8219f66dfc22bb020afe65ea0452ed
PitchBlack <mail@rudolfvonrohr.net>
2016-01-09 17:09:18 +0100

    Dokumentation angepasst

--- src/IMLVM/src/ch/fhnw/cpib/vm/RatioTest.java
+++ src/IMLVM/src/ch/fhnw/cpib/vm/RatioTest.java
@@ -5,7 +5,7 @@ public class RatioTest {
 	public static void main(String[] args) {
 		
 		
-		Ratio r = new Ratio(378171782,1177818189);
+		Ratio r = new Ratio(-281,280);
 		
 		System.out.println(r.toString());
 
